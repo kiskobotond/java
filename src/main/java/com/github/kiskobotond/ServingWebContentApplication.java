6baf5abee23a0dbe422964ddab0b32709d4bd8a7
kiskobotond <boci2002@gmail.com>
2021-03-21 16:29:07 +0100

    spring hello_word

---
+++ src/main/java/com/github/kiskobotond/ServingWebContentApplication.java
@@ -0,0 +1,13 @@
+package com.github.kiskobotond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServingWebContentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServingWebContentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/kiskobotond/controller/GreetingControllerTest.java
@@ -0,0 +1,32 @@
+package com.github.kiskobotond.controller;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class HttpRequestTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void greetingShouldReturnDefaultMessage() {
+        //given
+
+        //when
+        String result = this.restTemplate.getForObject(String.format("http://localhost:%d/greeting", port), String.class);
+
+        //then
+        assertThat(result).contains("Hello, World");
+    }
+}
